a5434b66427bd17f6a93bbf09884976a9cee7cfb
skang15 <skang15@berklee.edu>
2026-08-03 17:23:47 -0400

    Complete access Granted

---
+++ demo.py
@@ -0,0 +1,17 @@
+print("Hello, World!")
+
+name = "Sara"
+
+if name == "Sara":
+    print("Hey Sara")
+
+else:
+    print("Hey NOT Sara")
+
+
+# These are mony notes in Python
+
+def singHappyBirthday(name):
+    print("Happy birthday to you!")
+    print("Happy birthday to you!")
+    print("Happy birthday dear " + name)
